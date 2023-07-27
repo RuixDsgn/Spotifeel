404cc266b50907d1ef2c6ca9fc408887e19ce5c7
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-27 08:40:46 -0700

    added initial tables and models

--- server/app.py
+++ server/app.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 # Standard library imports
+import os
+from flask import Flask, request, make_response, session, jsonify
+from flask_migrate import Migrate
+from sqlalchemy import func
+from requests import get
 
 # Remote library imports
 from flask import request
@@ -8,9 +13,16 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User, Questionnaire, Mood, Playlist
 
-# Views go here!
+class Songs(Resource):
+    def get(self):
+        bearer_token = 'BQCZ6Tb9J2i7BcKPWT8a8IdLNaT-mcLJDdJsvq1rKZY6zYnyS4xt8t60fzFts_TVMue4g2ju-wY0S1EDli_sOMNUwFUxTkdyyk2DtluUEBHVQGTtItQ'
+        response = get('https://api.spotify.com/v1/tracks/5Tlwkpy7AU3yMXydeFwhCp?si=d6a8dc7941524d56',
+                    {'headers': {'Authorization': f'Bearer {bearer_token}'}})
+        return response.json()
+
+api.add_resource(Songs, '/song')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,35 @@
 from sqlalchemy_serializer import SerializerMixin
-
+from datetime import datetime
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    questionnaire = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    mood = db.Column(db.Integer, db.ForeignKey('moods.id'))
+    playlist = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Questionnaire(db.Model, SerializerMixin):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question = db.Column(db.String)
+    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Mood(db.Model, SerializerMixin):
+    __tablename__ = 'moods'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    mood = db.Column(db.String)
+
+
+class Playlist(db.Model, SerializerMixin):
+    __tablename__ = 'playlists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    mood_id = db.Column(db.Integer, db.ForeignKey('moods.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
